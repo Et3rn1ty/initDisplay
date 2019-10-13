bffb4d1dfc411743b0e2ab59890b2e658701c7ff
Doug <dougwein@gmail.com>
2019-10-13 18:59:24 -0400

    Update initDisplay.sh

--- initDisplay.sh
+++ initDisplay.sh
@@ -7,7 +7,7 @@ HDMI_NAME=`cat hdmi.name`
 echo $HDMI_NAME
 if [ "$HDMI_NAME" == "[E] No device present" ]; then
 	LCD_ON=`cat /boot/config.txt | grep "hdmi_drive=2"`
-	if [ $LCD_ON == "hdmi_drive=2" ]; then
+	if [[ $LCD_ON == "hdmi_drive=2" ]]; then
 			echo "reboot avec la configuration LCD"
 			sudo rm -f /boot/config.txt
 			sudo cp /boot/config_lcd.txt /boot/config.txt
@@ -17,7 +17,7 @@ if [ "$HDMI_NAME" == "[E] No device present" ]; then
 else
 	HDMI_ON=`cat /boot/config.txt | grep "lcd_rotate=2"`
 	echo $HDMI_ON
-	if [ $HDMI_ON == "lcd_rotate=2" ]; then
+	if [[ $HDMI_ON == "lcd_rotate=2" ]]; then
 			echo "reboot avec la configuration HDMI"
 			sudo rm -f /boot/config.txt
 			sudo cp /boot/config_hdmi.txt /boot/config.txt
